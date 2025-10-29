eef7df83790ce4a94bde054e6400849f058b5ba5
tejjm <uttej.madhusudhan@gmail.com>
2025-10-29 03:51:21 +0000

    Created the basic GUI

---
+++ main.py
@@ -0,0 +1,30 @@
+import pygame
+from constants import *
+def main():
+    pygame.init()
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    screen_on = True
+    while screen_on:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(color=(0,0,0))
+        pygame.display.flip()
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
