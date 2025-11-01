1681cd5134b224e7904f1828c4e0f5dc7d03397c
tejjm <uttej.madhusudhan@gmail.com>
2025-11-01 11:49:16 +0000

    Added asteroids spwaning

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable,drawable)
+    asteroids = pygame.sprite.Group()
+    Asteroid.containers = (asteroids,updatable,drawable)
+    AsteroidField.containers = (updatable)
     pygame.init()
+    asteroidfeild = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,12 +28,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color=(0,0,0))
-        player.draw(screen)
+        # player.draw(screen)
+        for drawablee in drawable:
+            drawablee.draw(screen)
         pygame.display.flip()
         delta_time = clock.tick(60)
         dt = delta_time/1000
-        player.update(dt=dt)
-
+        #player.update(dt=dt)
+        updatable.update(dt)
 
 
 
